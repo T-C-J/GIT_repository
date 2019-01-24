7844e17ca24bffaeda99e3e1204f418456b64173
jieming.yu <jieming.yu@counect.com>
2019-01-24 11:13:37 +0800

    测试

---
+++ src/test/java/com/tcj/startboot/StartbootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tcj.startboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
